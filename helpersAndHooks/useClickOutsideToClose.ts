791dc9e2ad35413171a4370438b193364e8baa60
jaw162 <jwhite162@protonmail.com>
2023-06-09 14:41:25 +0100

    Used normal prettier settings

--- helpersAndHooks/useClickOutsideToClose.ts
+++ helpersAndHooks/useClickOutsideToClose.ts
@@ -1,4 +1,4 @@
-import { useEffect, useCallback } from "react"
+import { useEffect, useCallback } from "react";
 
 export function useClickOutsideToClose<T>(
   setIsOpen: (arg: boolean) => void,
@@ -6,16 +6,16 @@ export function useClickOutsideToClose<T>(
 ) {
   const close = useCallback(
     (e: MouseEvent) => {
-      const shouldIgnore = refsToIgnore.some((el) => el.current === e.target)
-      if (shouldIgnore) return
-      setIsOpen(false)
+      const shouldIgnore = refsToIgnore.some((el) => el.current === e.target);
+      if (shouldIgnore) return;
+      setIsOpen(false);
     },
     [setIsOpen, refsToIgnore]
-  )
+  );
 
   useEffect(() => {
-    document.body.addEventListener("click", close)
+    document.body.addEventListener("click", close);
 
-    return () => document.body.removeEventListener("click", close)
-  }, [close])
+    return () => document.body.removeEventListener("click", close);
+  }, [close]);
 }
